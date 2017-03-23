946a4c08b3b8ff87b2e8b21c94504323b1d1fda6
likewater <lakebayomackway@gmail.com>
2017-03-23 11:33:35 -0700

    add code for fourth test.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,17 @@
 public class Task {
   private String mDescription;
+  private boolean mCompleted;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -20,5 +21,11 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals(false, myTask.isCompleted());
 }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
 
 }
