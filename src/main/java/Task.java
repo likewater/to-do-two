994ff48903c2825fb910c9ea95a151d958ce57e3
likewater <lakebayomackway@gmail.com>
2017-03-27 09:55:14 -0700

    add test to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.sql2o.*;
 
 public class Task {
   private String description;
@@ -15,6 +16,13 @@ public class Task {
     createdAt = LocalDateTime.now();
     // instances.add(this);
     // mId = instances.size();
+  }
+
+  public static List<Task> all() {
+    String sql = "SELECT id, description FROM tasks";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
   }
 
   public String getDescription() {
@@ -43,6 +51,16 @@ public class Task {
 
   public static Task find(int id) {
   //return instances.get(id - 1);
+  }
+
+  @Override
+  public boolean equals(Object otherTask) {
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription());
+    }
   }
 
 }
